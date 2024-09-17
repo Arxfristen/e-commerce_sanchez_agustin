4cde410e2d418a6522f157a5aa23580d52b1cb4a
Pepe <pepe@gmail.com>
2024-09-17 19:42:43 -0300

    second commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 console.log("Hola Mundo")
 
-document.querySelector('h1').textContent = "Productos";  // Añadir texto "Productos" en el h1
+document.querySelector('h1').textContent = "Productos";  
 
-// Seleccionamos el section con clase container
+
 const container = document.querySelector('section.container');
 
-// Crear un array vacío fuera del for
+
 let cardsArray = [];
 
-// Usar un for que comienza en 1 para generar las cards dinámicamente
-for (let i = 1; i <= 5; i++) {  // Puedes cambiar el valor 5 por la cantidad de cards que quieras generar
+
+for (let i = 1; i <= 5; i++) {  
   let card = `
     <div class="card">
       <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" alt="Producto ${i}">
@@ -20,9 +20,9 @@ for (let i = 1; i <= 5; i++) {  // Puedes cambiar el valor 5 por la cantidad de 
     </div>
   `;
 
-  // Agregar la card al array con push
+ 
   cardsArray.push(card);
 }
 
-// Asignar las cards generadas al innerHTML de la section
+
 container.innerHTML = cardsArray.join('');
